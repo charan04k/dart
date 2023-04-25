7f7794df16c52b4fc4f9b493e0079f7d7c2473df
charan04k <charankarthick04@gmail.com>
2023-04-25 14:16:40 +0530

    first commit

---
+++ first.dart
@@ -0,0 +1,14 @@
+void main()
+{
+    print("f");
+    var x=true;
+    print(x.runtimeType); // to check the data type of X
+    var num =123;
+    var name="charan";
+    var live=true;
+    var y;
+    print(live);
+    print(name);
+    print(num);
+    print(y);// to check the default data type
+}
