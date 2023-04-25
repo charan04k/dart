f4172e1581007c27f4e0917632839b72a2a51551
charan04k <charankarthick04@gmail.com>
2023-04-25 15:57:02 +0530

    end  commit of april 25

--- first.dart
+++ first.dart
@@ -7,7 +7,7 @@ void main()
     int num1=123;
     var name="charan";
     var live=true;
-    var y;
+    var y=10;
     print(live);
     print(name);
     print(num);
@@ -15,4 +15,16 @@ void main()
     print(num1.runtimeType);
     print(num==num1);// checking 
     // all the data type in dart are object . so the default value is NULL
+
+    // escape character
+    var s='it\'s me';
+    print(s);
+    int a=9;
+    double b=7; 
+    // String interpolation
+    var s2="karthick";
+//    var s3="My name is "+s2+name;// instead of using + we use $ that is string interpolation
+    var  s3="My name is $name";
+    print(s3);
+    print("${name} ${num} ${y.runtimeType}");
 }
