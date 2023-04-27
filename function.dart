3ab510138b765248d9c3e8d9085581c1e1e70c9b
charan04k <charankarthick04@gmail.com>
2023-04-27 17:35:59 +0530

    datatypes commit

--- function.dart
+++ function.dart
@@ -1,12 +1,24 @@
 void main()
 {
 perimeter();
-
+perimeter1(12,13);
+int are=area(20,30);
+print("area of reactangle is ${are}");
 }
-void perimeter()
+void perimeter()// no parameter function
 {
   int length=20;
   int breadth=30;
-  int perimeter =2+(length*breadth);
-  print(perimeter);
+  int perimeter =2*(length+breadth);
+  print("The perimeter of rectangle is ${perimeter}");
+}
+void perimeter1(int length,int breadth) //function witj par  int perimeter =2+(length*breadth);
+{
+  int perimeter =2*(length+breadth);
+  print("The perimeter of rectangle is ${perimeter}" );
+}
+
+int area(int length,int breadth)
+{
+  return length*breadth;
 }
