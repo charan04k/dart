7c2e564ac2af4c4a39d3f775d3bb8739f5c8a13f
charan04k <charankarthick04@gmail.com>
2023-04-28 09:43:54 +0530

    Scopes in dart

--- function.dart
+++ function.dart
@@ -1,24 +1,43 @@
-void main()
-{
-perimeter();
-perimeter1(12,13);
-int are=area(20,30);
-print("area of reactangle is ${are}");
-}
-void perimeter()// no parameter function
-{
-  int length=20;
-  int breadth=30;
-  int perimeter =2*(length+breadth);
-  print("The perimeter of rectangle is ${perimeter}");
-}
-void perimeter1(int length,int breadth) //function witj par  int perimeter =2+(length*breadth);
-{
-  int perimeter =2*(length+breadth);
-  print("The perimeter of rectangle is ${perimeter}" );
-}
 
-int area(int length,int breadth)
+// void main()
+// {
+
+// perimeter();
+// perimeter1(12,13);
+// int are=area(20,30);
+// print("area of reactangle is ${are}");
+
+// }
+
+// void perimeter()// no parameter function
+// {
+//   int length=20;
+//   int breadth=30;
+//   int perimeter =2*(length+breadth);
+//   print("The perimeter of rectangle is ${perimeter}");
+
+// }
+
+
+// void perimeter1(int length,int breadth) //function witj par  int perimeter =2+(length*breadth);
+// {
+//   int perimeter =2*(length+breadth);
+//   print("The perimeter of rectangle is ${perimeter}" );
+// }
+
+// int area(int length,int breadth)
+// {
+//   return length*breadth;
+// }
+
+
+// <-- example of lexical scope -->
+ void main() {
+  var x="charan";
+    void lexi()
 {
-  return length*breadth;
+  // var x="karthick";
+  print(x); 
+}  
+lexi();
 }
